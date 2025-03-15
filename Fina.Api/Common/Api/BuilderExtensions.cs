198b93471844b161a205a8d8802036fe9ea9834f
MatheusIleck <matheus.ileck556@gmail.com>
2025-03-14 21:22:29 -0300

    Ultimo commit antes da criação das transações

--- Fina.Api/Common/Api/BuilderExtensions.cs
+++ Fina.Api/Common/Api/BuilderExtensions.cs
@@ -1,64 +1,66 @@
-﻿using Fina.Api.Data;
+﻿
+using Fina.Api.Data;
 using Fina.Api.Handlers;
 using Fina.Core;
 using Fina.Core.Handlers;
 using Microsoft.EntityFrameworkCore;
 
-namespace Fina.Api.Common.Api
+namespace Fina.Api.Common.Api;
+
+public static class BuildExtension
 {
-    public static class BuilderExtensions
+    public static void AddConfiguration(this WebApplicationBuilder builder)
     {
-        public static void AddConfiguration(this WebApplicationBuilder builder)
-        {
-            ApiConfiguration.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
-            Configuration.BackendUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
-            Configuration.FrontendUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
+        ApiConfiguration.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+        Configuration.BackendUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
+        Configuration.FrontendUrl = builder.Configuration.GetValue<string>("FrontendUrl") ?? string.Empty;
+    }
 
-        }
-        public static void addDocumentation(this WebApplicationBuilder builder)
+    public static void AddDocumentation(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(x =>
         {
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(x =>
-            {
-                x.CustomSchemaIds(n => n.FullName);
+            x.CustomSchemaIds(n => n.FullName);
+        });
+    }
 
-            });
-        }
-        public static void addDataContexts(this WebApplicationBuilder builder)
-        {
-            builder
-                .Services
-                .AddDbContext<AppDbContext>(
+    public static void AddDataContexts(this WebApplicationBuilder builder)
+    {
+        builder
+            .Services
+            .AddDbContext<AppDbContext>(
                 x =>
                 {
                     x.UseSqlServer(ApiConfiguration.ConnectionString);
                 });
-        }
 
-        public static void AddCrossOrigin(this WebApplicationBuilder builder)
-        {
-            builder.Services.AddCors(
-                options => options.AddPolicy(
-                    ApiConfiguration.CorsPolicyName,
-                    policy => policy
+    }
+
+    public static void AddCrossOrigin(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddCors(
+            options => options.AddPolicy(
+                ApiConfiguration.CorsPolicyName,
+                policy => policy
                     .WithOrigins([
                         Configuration.BackendUrl,
                         Configuration.FrontendUrl
-                        ])
+                    ])
                     .AllowAnyMethod()
                     .AllowAnyHeader()
                     .AllowCredentials()
-                    ));
-        }
+            ));
+    }
 
-        public static void AddServices(this WebApplicationBuilder builder)
-        {
-            builder
-                .Services
-                .AddTransient<ICategoryHandler, CategoryHandler>();
-            builder
-                .Services
-                .AddTransient<ITransactionHandler, TransactionHandler>();
-        }
+    public static void AddServices(this WebApplicationBuilder builder)
+    {
+        builder
+            .Services
+            .AddTransient<ICategoryHandler, CategoryHandler>();
+
+        builder
+            .Services
+            .AddTransient<ITransactionHandler, TransactionHandler>();
     }
 }
--- Fina.Api/Program.cs
+++ Fina.Api/Program.cs
@@ -1,25 +1,20 @@
-
 using Fina.Api;
 using Fina.Api.Common.Api;
 using Fina.Api.Endpoints;
+using Fina.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddConfiguration();
-builder.addDataContexts();
+builder.AddDataContexts();
 builder.AddCrossOrigin();
-builder.addDocumentation();
+builder.AddDocumentation();
 builder.AddServices();
-
-
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
     app.ConfigureDevEnvironment();
 
-
 app.UseCors(ApiConfiguration.CorsPolicyName);
 app.MapEndPoints();
 
 app.Run();
-
-
